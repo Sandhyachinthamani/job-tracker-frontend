3ec20259ed9b76b628e9004e8676291b2a9d2358
sandhyaChinthamani <sandhya441996@gmail.com>
2025-04-24 10:25:24 +0530

    railway backend commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://job-tracker-backend-production.up.railway.app/api"
+  baseURL: "https://job-tracker-backend-production-1db0.up.railway.app/"
 });
 
 export default API;
